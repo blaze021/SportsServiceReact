f6b0b35f1dc48d40b1f927f6f6278e0dd4d4a48e
TarunBlaze <40628308+blaze021@users.noreply.github.com>
2020-07-26 19:55:54 +0530

    Update firebase.js

--- src/components/infocomponents/firebase.js
+++ src/components/infocomponents/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-	apiKey: 'AIzaSyCQg2cuGIbbmlAHrWidvJeReqDs7Pe_3sM',
+	apiKey: 'YOUR_API_KEY',
 	authDomain: 'fitom-site.firebaseapp.com',
-	databaseURL: 'https://fitom-site.firebaseio.com',
-	projectId: 'fitom-site',
-	storageBucket: 'fitom-site.appspot.com',
-	messagingSenderId: '78257898162',
-	appId: '1:78257898162:web:fa5c0fa0e8f89e1389ffab'
+	databaseURL: 'YOUR_DB_URL',
+	projectId: 'PROJECT_ID',
+	storageBucket: 'YOUR_STORAGE_BUCKET',
+	messagingSenderId: 'YOUR_MESSAGE_SENDER_ID',
+	appId: 'YOUR_API_ID'
 });
 
 const db = firebaseApp.firestore();
